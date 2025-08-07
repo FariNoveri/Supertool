32ae6327f3da21f80bebd7d4e7123bca56b20c89
Fari Noveri <cahayalunamaharani1@gmail.com>
2025-08-07 09:45:46 +0700

    tes

--- MainLoader.lua
+++ MainLoader.lua
@@ -12,6 +12,6 @@ print("[Supertool] ScreenGui created")
 local frame = Instance.new("Frame")
 frame.Size = UDim2.new(0, 200, 0, 100)
 frame.Position = UDim2.new(0.5, -100, 0.5, -50)
-frame.BackgroundColor3 = Color3.fromRGB(255, 0, 0) -- Merah biar gampang kelihatan
+frame.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
 frame.Parent = screenGui
 print("[Supertool] Frame created")
