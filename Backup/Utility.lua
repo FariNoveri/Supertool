f693289ed269d1a32350ab0b6f6c56deea44aebe
Fari Noveri <cahayalunamaharani1@gmail.com>
2025-09-13 08:45:36 +0700

    utility.lua add change

--- Backup/Utility.lua
+++ Backup/Utility.lua
@@ -1934,7 +1934,7 @@ function Utility.loadUtilityButtons(createButton)
         loadAdonisBypass()
     end)
     createButton("Load Kohl's Admin", function()
-        loadstring(game:HttpGet("https://pastebin.com/raw/wBmP410Q"))()
+        loadstring(game:HttpGet("https://pastebin.com/raw/MhS1eXY9"))()
     end)
     end
 
